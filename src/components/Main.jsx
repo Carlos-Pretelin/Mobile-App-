878c493975813f74af6d51e686e2c1e9e0b89b38
Carlos Pretelin <carlosppretelin@gmail.com>
2022-12-24 08:41:09 -0600

    update to second section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import "../styles/Main.scss"
 
 const Main = () => {
   return (
     <main>
-        <section>Seccion 1</section>
+        <section className='Main_exchange-container'>
+          <div className='backgroundImg'></div>
+          <div className='Main_exchange-container--title'>
+
+              <h2>Visibilizamos todas las tasas de cambio</h2>
+              <p>Traemos informacion en tiempo real de las casas de 
+                cambio y las monedas mas importantes del mundo
+              </p>
+              
+          </div>
+          <section className='main-tables-container'>
+            <div>
+              
+            </div>
+          </section>
+        </section>
         <section>Seccion 2</section>
         <section>Seccion 3</section>
     </main>
